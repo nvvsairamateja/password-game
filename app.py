bc087178e9a35a034fcd15304051592158d5042c
nvvsairamateja <47604273+nvvsairamateja@users.noreply.github.com>
2023-07-31 01:26:48 +0530

    adding app.py file

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('pasword.html')
+
+@app.route('/check_password_strength', methods=['POST'])
+def check_password_strength():
+    password = request.form['password']
+    password_length = len(password)
+
+    if password_length >= 8:
+        response = {'message': 'Password is strong!'}
+    else:
+        response = {'message': 'Password is weak. It should be at least 8 characters long.'}
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(debug=True)
