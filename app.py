ee5dded97709800580ea9b26cedd8936054fef11
nvvsairamateja <47604273+nvvsairamateja@users.noreply.github.com>
2023-08-08 01:51:56 +0530

    level 7 added

--- app.py
+++ app.py
@@ -9,27 +9,30 @@ def index():
     return render_template("pasword.html")
 
 
-def is_month(password):
+def contains_month(password):
     months = 'january|february|march|april|may|june|july|august|september|october|november|december'
     return re.search(months, password, re.IGNORECASE)
 
 
-def check_special_char(password):
+def contains_special_char(password):
     regex = re.compile('[@_!#$%^&*()<>?/\|}{~:]')
     if(regex.search(password) == None):
-        return True
-    else:
         return False
+    else:
+        return True
 
 
 def sum_of_digits(password):
-    digits = []
     total = 0
     for chr in password:
         if chr.isdigit():
-            digits.append(int(chr))
-    total = sum(digits)
+            total+=(int(chr))
     return total
+
+def contains_roman(password):
+    romans = 'I|V|X|L|C|D|M'
+    return re.search(romans, password)
+
 
 
 @app.route('/check_password_strength', methods=['POST'])
@@ -43,12 +46,14 @@ def check_password_strength():
         response = {'message': 'Your password must include an uppercase letter.'}
     elif not any(chr.isdigit() for chr in password):
         response = {'message': 'Your password must include a number.'}
-    elif check_special_char(password):
+    elif not contains_special_char(password):
         response = {'message': 'Your password must include a special character.'}
     elif sum_of_digits(password) != 25:
         response = {'message': 'The digits in your password must add up to 25.'}
-    elif not is_month(password):
+    elif not contains_month(password):
         response = {'message': 'Your password must include a month of the year.'}
+    elif not contains_roman(password):
+        response = {'message': 'Your password must include a roman numeral.'}
     else:
         response = {'message': 'Password is strong!'}
 
